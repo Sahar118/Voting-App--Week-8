af59bcff2950c11bebc73af8d54ef5eca21c4ca0
sahar118 <sahar.t118@gmail.com>
2023-02-17 10:52:14 +0200

    add styles component

---
+++ voting-app/src/Pages/Login.page.js
@@ -0,0 +1,20 @@
+// import Wrapper  from '../styles/styled/Login.styled'
+// import LoginStyle from '../styles/styled/Login.styled';
+import Logo from '../Component/Logo.components'
+
+const Login = () => {
+    return (
+        <>
+
+            <form className="form">
+                <Logo />
+                <h1 className='title-login-page'>Kids Choice Awards</h1>
+
+            </form>
+
+        </>
+    );
+};
+
+
+export default Login;
---
+++ voting-app/src/Pages/index.js
@@ -0,0 +1,2 @@
+import Login from "./Login.page";
+export { Login };
